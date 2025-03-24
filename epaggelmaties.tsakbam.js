56a7b3b07a0492bb9bc2c9196e01137ae39a9930
giannis.sarantopoulos <giannis.sarantopoulos@hellasdirect.gr>
2025-03-24 17:36:21 +0200

    update tsakbam

--- epaggelmaties.tsakbam.js
+++ epaggelmaties.tsakbam.js
@@ -34,24 +34,41 @@ function copyTextToClipboard(text) {
 }
 
 function getCompanyData() {
-    const allCompanies = document.querySelectorAll('#content > div.cells.three > section .cell-data');
+    const allCompanies = document.querySelectorAll('#results-container > div');
     let output = '';
     allCompanies.forEach(company => {
         if (output !== '') {
             output += "\n\n";
         }
-        output += company.querySelector('[itemprop="name"]').innerText + "\n";
-        output += company.querySelector('address').innerText.replace("\n\n", " ") + "\n";
-        output += Array.from(company.querySelectorAll('[itemprop="telephone"]')).filter(tel => tel.innerText !== '').map(tel => tel.innerText) + "\n";
-
-        const ps = company.querySelectorAll('p');
-        output += ps[ps.length - 1].innerText;
+        output += company.querySelector('.info h3').innerText + "\n";
+        output += company.querySelector('.info-item.d-flex:nth-child(2)').innerText.replaceAll("\n\n", " ") + "\n"
+        output += company.querySelector('.info-item.d-flex:nth-child(3)').innerText.replaceAll("\n\n", " ") + "\n"
+        output += document.querySelector('.section-header').innerText.split(' ')[0];
     });
 
     console.log(output);
     copyTextToClipboard(output);
 }
 
+let content = '';
+async function goThroughPages() {
+    let previousLoadedCompanies = 0;
+    let loadedCompanies = document.querySelectorAll('#results-container > div');
+    const loadMoreBtn = document.getElementById("load-more-btn");
+
+    while (await new Promise(resolve => setTimeout(() => {
+        loadedCompanies = document.querySelectorAll('#results-container > div');
+        const hasNewCompanies = previousLoadedCompanies < loadedCompanies.length;
+        previousLoadedCompanies = loadedCompanies.length;
+        resolve(hasNewCompanies)
+    }, 500))) {
+        loadMoreBtn.click();
+    }
+
+    content += getCompanyData();
+    console.log(content);
+    copyTextToClipboard(content);
+}
 
 let button = document.getElementById('myButton');
 
@@ -71,5 +88,5 @@ if (!button) {
     document.body.prepend(button);
 
     // Add event listener to button to trigger getData function on click
-    button.addEventListener('click', getCompanyData);
+    button.addEventListener('click', goThroughPages);
 }
--- readme.md
+++ readme.md
@@ -1,3 +1,1 @@
 1. [epaggelmaties.tsakbam](./epaggelmaties.tsakbam.md)
-2. [vres.business](./vres.business.md)
-3. [topodigos.gr](./topodigos.md)
